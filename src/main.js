a751bb5b6813adbd8971f8d7b9711239e5491c73
Dirty-Six <straubinger.k@gmail.com>
2024-02-25 16:34:58 +0100

    prevent console logging

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+if(new URL(location.href).searchParams.get('log') != 1) window.console.log = function() {};
+
 const app = createApp(App)
 
 app.use(createPinia())
